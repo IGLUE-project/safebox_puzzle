1d35c9c276de0d2593a2cd870ce572c5c56f7762
unknown <agordillos@gmail.com>
2026-01-22 14:25:53 +0100

    Serbian translation

--- src/config/locales.js
+++ src/config/locales.js
@@ -8,7 +8,7 @@ export let LOCALES = {
     "i.continue":"Continuar",
   },
   sr:{
-    "i.message": "You've opened the safebox! \n Click 'Continue' to advance to the next puzzle.",
-    "i.continue":"Continue",
+    "i.message": "Otvorili ste sef! \n Kliknite na 'Nastavi' da biste prešli na sledeću zagonetku.",
+    "i.continue":"Nastavi",
   },
 };
